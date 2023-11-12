c38ceda0e6350730cecb0cc8ea239c9dcb8807b1
ZJTaggerung <70484575+ZJTaggerung@users.noreply.github.com>
2023-11-12 10:37:51 -0500

    Add files via upload

--- task1.py
+++ task1.py
@@ -26,14 +26,18 @@ def read_input():
 def alg1(n, k, cost):
     minimum = [-1] * (n + 1)
     minimum[0] = 0
+    minimum_order = [-1]
+    minimum_order[0] = 0
     if n == 0:
-        return minimum
+        return minimum, minimum_order
     else:
         for i in range(1, k + 1):
             if n - i >= 0:
-                minimum_placeholder = alg1(n - i, k, cost)
+                minimum_placeholder, minimum_placeholder_order = alg1(n - i, k, cost)
                 minimum_placeholder.append(minimum_placeholder[n - i] + cost[n - i])
+                minimum_placeholder_order.append(n)
                 if minimum[-1] == -1 or minimum[-1] > minimum_placeholder[-1]:
+                    minimum_order = minimum_placeholder_order
                     minimum[-1] = minimum_placeholder[-1]
                     minimum[-1 - i] = minimum_placeholder[-2]
                     j = 1
@@ -41,7 +45,7 @@ def alg1(n, k, cost):
                         minimum[-1 - i - j] = minimum_placeholder[-2 - j]
                         j += 1
 
-        return minimum
+        return minimum, minimum_order
 
 
 """
@@ -62,6 +66,9 @@ def alg1(n, k, cost):
 
 if __name__ == "__main__":
     n, k, cost = read_input()
-    minimum = alg1(n, k, cost)
+    minimum, minimum_order = alg1(n, k, cost)
     # print(minimum)
-    print(minimum[n])
+    # print(minimum[n])
+    # print(minimum_order)
+    for i in minimum_order:
+        print(i, '', end="")
--- task5.py
+++ task5.py
@@ -35,21 +35,26 @@ def read_input():
 
 def alg1(n, k, cost, m, final_n):
     minimum = [-1] * (n + 1)
+    minimum_order = [-1]
     if n == 0:
         if m == 0:
             minimum[0] = 0
-            return minimum
+            minimum_order[0] = 0
+            return minimum, minimum_order
         else:
-            return minimum
+            return minimum, minimum_order
     else:
         for i in range(1, k + 1):
             if n - i >= 0:
-                minimum_placeholder = alg1(n - i, k, cost, m - 1, final_n)
+                minimum_placeholder, minimum_placeholder_order = alg1(n - i, k, cost, m - 1, final_n)
                 if minimum_placeholder[-1] != -1:
                     minimum_placeholder.append(minimum_placeholder[n - i] + cost[n - i])
+                    minimum_placeholder_order.append(n)
                 else:
                     minimum_placeholder.append(-1)
+                    minimum_placeholder_order.append(-1)
                 if minimum_placeholder[-1] != -1 and (minimum[-1] == -1 or minimum[-1] > minimum_placeholder[-1]):
+                    minimum_order = minimum_placeholder_order
                     minimum[-1] = minimum_placeholder[-1]
                     minimum[-1 - i] = minimum_placeholder[-2]
                     j = 1
@@ -57,15 +62,18 @@ def alg1(n, k, cost, m, final_n):
                         minimum[-1 - i - j] = minimum_placeholder[-2 - j]
                         j += 1
 
-        return minimum
+        return minimum, minimum_order
 
 
 if __name__ == "__main__":
     n, k, cost, m = read_input()
     final_n = n
-    minimum = alg1(n, k, cost, m, final_n)
+    minimum, minimum_order = alg1(n, k, cost, m, final_n)
     # print(minimum)
     if minimum[n] == -1:
         print("No solution possible.")
     else:
-        print(minimum[n])
+        # print(minimum[n])
+        # print(minimum_order)
+        for i in minimum_order:
+            print(i, '', end="")
