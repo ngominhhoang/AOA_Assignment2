b52fe9312037aa442f650db3193e812675341be0
ZJTaggerung <70484575+ZJTaggerung@users.noreply.github.com>
2023-11-13 14:46:21 -0500

    Add files via upload

--- task1.py
+++ task1.py
@@ -1,4 +1,6 @@
+# Reads input
 def read_input():
+    # Reads user input for the number of platforms (n) and maximum jump length (k).
     correct_input = False
     while not correct_input:
         try:
@@ -8,6 +10,7 @@ def read_input():
             print("You must enter two integers. Please try again: ")
             continue
 
+    # Reads user input for the cost of jumping from each platform (cost).
     cost = [0] * n
     correct_input = False
     while not correct_input:
@@ -23,19 +26,26 @@ def read_input():
     return n, k, cost
 
 
+# Algorithm 1, brute-force algorithm
 def alg1(n, k, cost):
     minimum = [-1] * (n + 1)
     minimum[0] = 0
     minimum_order = [-1]
     minimum_order[0] = 0
     if n == 0:
+        # Base case
         return minimum, minimum_order
     else:
         for i in range(1, k + 1):
             if n - i >= 0:
+                # Recursive call, ultimately k sub-problems that decrease by as little as 1 each iteration. O(k^n)
                 minimum_placeholder, minimum_placeholder_order = alg1(n - i, k, cost)
+
+                # Potentially our new minimum and minimum order.
                 minimum_placeholder.append(minimum_placeholder[n - i] + cost[n - i])
                 minimum_placeholder_order.append(n)
+
+                # If our calculated minimum is smaller than our current minimum, we have a new solution.
                 if minimum[-1] == -1 or minimum[-1] > minimum_placeholder[-1]:
                     minimum_order = minimum_placeholder_order
                     minimum[-1] = minimum_placeholder[-1]
@@ -47,8 +57,31 @@ def alg1(n, k, cost):
 
         return minimum, minimum_order
 
+
+"""
+def alg1(n, k, cost):
+    minimum = [-1] * (n + 1)
+    minimum[0] = 0
+    for position in range(n):
+        for i in range(position + 1, position + k + 1):
+            if i > n:
+                continue
+            elif minimum[i] == -1 or minimum[position] + cost[position] < minimum[i]:
+                minimum[i] = minimum[position] + cost[position]
+
+    print(minimum)
+    print(minimum[n])
+    return minimum
+"""
+
+# Main function for running code.
 if __name__ == "__main__":
     n, k, cost = read_input()
     minimum, minimum_order = alg1(n, k, cost)
-    for i in minimum_order[0:-1]:
+    # print(minimum)
+    # print(minimum[n])
+    # print(minimum_order)
+
+    # Prints the platform jumping order for each platform.
+    for i in minimum_order[:-1]:
         print(i, '', end="")
--- task5.py
+++ task5.py
@@ -1,4 +1,6 @@
+# Reads input
 def read_input():
+    # Reads user input for the number of platforms (n), maximum jump length (k), and required number of jumps (m).
     correct_input = False
     while not correct_input:
         try:
@@ -8,6 +10,7 @@ def read_input():
             print("You must enter three integers. Please try again: ")
             continue
 
+    # Reads user input for the cost of jumping from each platform (cost).
     cost = [0] * n
     correct_input = False
     while not correct_input:
@@ -17,13 +20,29 @@ def read_input():
         except:
             print("You must enter integers. Please try again: ")
             continue
+
+    """
+    correct_input = False
+    while not correct_input:
+        try:
+            m = int(input())
+            correct_input = True
+        except:
+            print("You must enter an integer. Please try again: ")
+            continue
+    """
+
+    # print(n, k)
+    # print(cost)
+    # print(m)
     return n, k, cost, m
 
-
+# Algorithm 5, brute-force algorithm
 def alg5(n, k, cost, m, final_n):
     minimum = [-1] * (n + 1)
     minimum_order = [-1]
     if n == 0:
+        # Base case. If m is 0, we have the required number of jumps. Otherwise, it's not a viable solution.
         if m == 0:
             minimum[0] = 0
             minimum_order[0] = 0
@@ -33,13 +52,18 @@ def alg5(n, k, cost, m, final_n):
     else:
         for i in range(1, k + 1):
             if n - i >= 0:
+                # Recursive call, ultimately k sub-problems that decrease by as little as 1 each iteration. O(k^n)
                 minimum_placeholder, minimum_placeholder_order = alg5(n - i, k, cost, m - 1, final_n)
+
+                # Potentially our new minimum and minimum order. Ignored if not required number of jumps.
                 if minimum_placeholder[-1] != -1:
                     minimum_placeholder.append(minimum_placeholder[n - i] + cost[n - i])
                     minimum_placeholder_order.append(n)
                 else:
                     minimum_placeholder.append(-1)
                     minimum_placeholder_order.append(-1)
+
+                # If our calculated minimum is smaller than our current minimum, we have a new solution.
                 if minimum_placeholder[-1] != -1 and (minimum[-1] == -1 or minimum[-1] > minimum_placeholder[-1]):
                     minimum_order = minimum_placeholder_order
                     minimum[-1] = minimum_placeholder[-1]
@@ -52,15 +76,18 @@ def alg5(n, k, cost, m, final_n):
         return minimum, minimum_order
 
 
+# Main function for running code.
 if __name__ == "__main__":
     n, k, cost, m = read_input()
     final_n = n
     minimum, minimum_order = alg5(n, k, cost, m, final_n)
     # print(minimum)
+
+    # Prints the platform jumping order for each platform if a solution exists.
     if minimum[n] == -1:
         print("No solution possible.")
     else:
         # print(minimum[n])
         # print(minimum_order)
-        for i in minimum_order:
+        for i in minimum_order[:-1]:
             print(i, '', end="")
